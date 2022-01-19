d722e8384c7c297bbc2746e4154ab32db1363bbc
ltang106 <tanglh@g.cofc.edu>
2022-01-19 18:00:35 -0500

    completed lab1

---
+++ notes/Chapter 1 Notes.py
@@ -0,0 +1,18 @@
+
+
+def convert():
+    celsius = eval(input("What is the temperature in celsius? "))
+    fahrenheit = celsius * 9 / 5 + 32
+    # celsius * 9 / 5 + 32 is an expression and they produce the outputs
+    # my_variable = 7 or "hi" is a literal (no calculation)
+    print("The temperature is:", fahrenheit, "degrees fahrenheit.", end="---")
+
+convert()
+
+
+
+
+
+
+
+
